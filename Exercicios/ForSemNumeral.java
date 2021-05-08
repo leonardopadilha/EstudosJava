1b02b0c6e8a8ba3f7368b46a89885c039a99dfb5
leonardopadilha <leonardopadilha28@gmail.com>
2021-05-08 09:11:35 -0300

    ForSemNumeral

---
+++ Exercicios/ForSemNumeral.java
@@ -0,0 +1,7 @@
+public class UsoFor {
+    public static void main(String[] args) {
+        for (String v = "#"; !v.equals("#####"); v +="#") {
+            System.out.println(v);
+        }
+    }
+}
